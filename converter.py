c9ae5ea9188f5b72e76a89c8614e4493360eb554
jomam23 <126360979+jomam23@users.noreply.github.com>
2024-03-24 09:20:01 -0700

    this is a scrambler script/unscrambler

---
+++ converter.py
@@ -0,0 +1,23 @@
+var= input("words up to 16 ")
+nums = int(input("input key "))
+print(var[nums::-1])
+
+var2 = input("do you want to unscrambel y/n ")
+
+if var2 == "y":
+    print("okay have fun")
+    var3 = input(" stuff ")
+    print(var3[nums::-1])
+
+
+
+
+
+
+
+
+
+if var2 == "n":
+    print("good bye!")
+    print("(we dident like you anyway)")
+#made my jomoma23
